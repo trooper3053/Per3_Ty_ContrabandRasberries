22dc43ae3f95b5c8fc9e1f81eee5b3219a34b6e1
trooper3053 <trooper3053@gmail.com>
2018-05-29 19:59:25 +0000

    beginning

---
+++ proyecto.py
@@ -0,0 +1,12 @@
+import random
+import time
+
+rounds = 0
+
+print("The round is about to start")
+
+while True:
+    print("Green Light")
+    time.sleep(random.randint(1000,10000)/1000.0)
+    
+    
