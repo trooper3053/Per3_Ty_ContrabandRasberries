828e66b3e5529ae6083c804c13f5097354460c02
trooper3053 <trooper3053@gmail.com>
2018-05-29 20:37:47 +0000

    defining buttons

--- proyecto.py
+++ proyecto.py
@@ -1,12 +1,25 @@
-import random
 import time
+import gpiozero as gpiozero
 
-rounds = 0
+def go_button_press():
+    gpiozero.setmode(gpiozero.BCM)
+    gpiozero.setup(21,gpiozero.IN)
+    if gpiozero.input(21):
+        print("Go")
+        return True
+    else:
+        
+        return False
 
-print("The round is about to start")
 
-while True:
-    print("Green Light")
-    time.sleep(random.randint(1000,10000)/1000.0)
-    
+def stop_button_press():
+    gpiozero.setmode(gpiozero.BCM)
+    gpiozero.setup(23,gpiozero.IN)
+    if gpiozero.input(23):
+        print("stop")
+        return True
+    else:
+        return False
+
+
     
