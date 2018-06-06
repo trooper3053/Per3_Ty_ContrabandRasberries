b99f55afa8845227d0ab11694a8d4c82d8d6691d
trooper3053 <trooper3053@gmail.com>
2018-06-06 20:10:15 +0000

    Finish definitions

--- proyecto.py
+++ proyecto.py
@@ -1,23 +1,37 @@
 import time
+from time import sleep
 import gpiozero as gpiozero
-from gpiozero import Button
-button= Button(23)
+from gpiozero import Button, LED
 
+stop_button= Button(23)
+go_button= Button(21)
+led_green= LED(12)
+led_red= LED(25)
 
 def go_button_press():
-    
-    pass
+    return go_button.is_pressed
     
 
 
 def stop_button_press():
-    return button.is_pressed
+    return stop_button.is_pressed
 
 
 
 
 def green_led_on():
-    pass
+    led_green.on()
+    
+def green_led_off():
+    led_green.off()
 
-while True:
-    print(stop_button_press())
+def red_led_on():
+    led_red.on()
+    
+def red_led_off():
+    led_red.off()
+
+
+green_led_on()
+sleep(1)
+green_led_off()
